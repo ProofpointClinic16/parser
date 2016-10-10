2c2117f1e811f86d018d08273fc34a6b783b9156
VOjha <vidushi.ojha@gmail.com>
2016-10-09 20:05:24 -0700

    Clean parser file, gitignore

--- parser.py
+++ parser.py
@@ -1,19 +1,25 @@
 import json
 from pprint import pprint
 
-with open("sample.txt") as f:
-    for line in f:
-        line = line.replace("u'","\"")
-        line = line.replace("u\"", "\"")
-        line = line.replace("':", "\":")
-        line = line.replace("',", "\",")
-        line = line.replace("']", "\"]")
-        line = line.replace("'}", "\"}")
-        line = line.replace(" date", " \"date")
-        line = line.replace("),", ")\",")
-        line = line.replace(")',", ")\",")
-        line = line.replace("None", "\"None\"")
-        line = line.replace("Object", "\"Object")
 
-        data = json.loads(line)
-        pprint(data)
+def parse(filename):
+    with open(filename) as f:
+        for line in f:
+            line = line.replace("u'","\"")
+            line = line.replace("u\"", "\"")
+            line = line.replace("':", "\":")
+            line = line.replace("',", "\",")
+            line = line.replace("']", "\"]")
+            line = line.replace("'}", "\"}")
+            line = line.replace(" date", " \"date")
+            line = line.replace("),", ")\",")
+            line = line.replace(")',", ")\",")
+            line = line.replace("None", "\"None\"")
+            line = line.replace("Object", "\"Object")
+
+            data = json.loads(line)
+            pprint(data)
+
+
+def test():
+    parse('sample.txt')
